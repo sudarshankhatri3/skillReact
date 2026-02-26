396a4ee144d10f5e6c63761f861adf5f2a8078b2
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-02-26 19:31:19 +0545

    add some log

--- src/component/common/hookPractice.jsx
+++ src/component/common/hookPractice.jsx
@@ -6,17 +6,21 @@ export default function CounterData(){
     const [count,setCount]=useState(0)
 
     function incrementer(){
+        console.log(`Before increment:${count}`)
         setCount((value)=>value+1)
+        console.log(`After count:${count}`)
     }
     function decrementer(){
+        console.log(`Before increment:${count}`)
         setCount((value)=>value-1)
+        console.log(`After decrement:${count}`)
     }
 
 
     return (
         <>
           <button onClick={incrementer}>Increment</button>
-          <h3>count</h3>
+          <h3>{count}</h3>
           <button onClick={decrementer}>Decrement</button>          
         </>
     )
